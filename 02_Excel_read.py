0b5b859e173edc99dba683735d476c344891faca
office-dell <kimyangsob@naver.com>
2023-07-11 16:54:19 +0900

    2023-07-11 3

--- 02_Excel_read.py
+++ 02_Excel_read.py
@@ -1,3 +1,8 @@
+# 패키지 설치
+# PS C:\Users\dell\python> pip install openpyxl
+# PS C:\Users\dell\python> pip install pandas
+# PS C:\Users\dell\python> pip install xlwings
+
 import openpyxl    # 공식문서 https://openpyxl.readthedocs.io/en/stable/
 
 fpath = r'C:\Users\dell\python\Excel_make.xlsx'
